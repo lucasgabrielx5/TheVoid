43427a76d84eacf7d0f8d7f802a58bf688c3cf80
lucasgabrielx5 <60987409+lucasgabrielx5@users.noreply.github.com>
2020-02-12 19:06:57 -0300

    Add files via upload

---
+++ Addition/src/addition/Addition.java
@@ -0,0 +1,23 @@
+
+package addition;
+import javax.swing.JOptionPane;
+
+public class Addition {
+
+    public static void main(String[] args) {
+        String firstNumber =
+            JOptionPane.showInputDialog("Enter First Integer");
+        String secondNumber =
+            JOptionPane.showInputDialog("Enter Second Integer");
+    
+        int number1 = Integer.parseInt(firstNumber);
+        int number2 = Integer.parseInt(secondNumber);
+        
+        int sum = number1 + number2;
+        
+        JOptionPane.showMessageDialog(null, "The sum is "+sum,
+                "Sum of Two Integers",JOptionPane.PLAIN_MESSAGE);
+        
+    }
+    
+}
